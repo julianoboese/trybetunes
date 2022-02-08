7c1552ed63d9e7bcd2cea5f7e364dad1a9a2b140
Juliano Boese <juliano.boese@gmail.com>
2022-02-07 23:46:42 -0300

    Estiliza página de Not Found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../assets/logo-login.svg';
+import './css/NotFound.css';
 
 function NotFound() {
-  return <div data-testid="page-not-found">NotFound</div>;
+  return (
+    <div className="page-not-found" data-testid="page-not-found">
+      <Logo />
+      <div className="not-found-container">
+        <p className="ops">Ops!</p>
+        <p className="not-found">A página que você está procurando não foi encontrada.</p>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
